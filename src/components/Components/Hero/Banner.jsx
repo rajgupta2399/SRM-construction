99d388ef79fe4e6fb1818591f8866644914ec6d0
rajgupta2399 <rajkumardotnet2499@gmail.com>
2024-12-19 23:44:40 +0530

    logoChangges

--- src/components/Components/Hero/Banner.jsx
+++ src/components/Components/Hero/Banner.jsx
@@ -10,7 +10,7 @@ Complete Waterproofing solutions for your Home.Let's waterproof your architectur
 const HomeCoverSection = ({ blogs }) => {
   return (
     <div className="w-full inline-block">
-      <article className="flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]">
+      <article className="flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[65vh] sm:h-[85vh]">
         <div
           className="absolute top-0 left-0 bottom-0 right-0 h-full
             bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0
--- src/components/Components/Icons/Logo.jsx
+++ src/components/Components/Icons/Logo.jsx
@@ -5,14 +5,14 @@ import logo from "../../../../public/assets/logo.png"
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark">
-      <div className="w-16 h-5 md:w-32 md:h-auto mr-2 md:mr-4">
+      <div className="w-14 h-9 md:w-32 md:h-auto mr-2 md:mr-4">
         <Image
           src={logo}
           alt="CodeBucks logo"
           layout="intrinsic"
           unoptimized
           width={60} // Intrinsic width of the original image
-          height={30} // Intrinsic height of the original image
+          height={60} // Intrinsic height of the original image
         />
       </div>
       {/* <span className="font-bold dark:font-semibold text-lg md:text-xl">
